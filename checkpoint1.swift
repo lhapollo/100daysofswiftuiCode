406117e9831a20758fa9f67aafc863726c65063d
Lexi Han <108366350+lhapollo@users.noreply.github.com>
2024-04-01 10:59:53 -0400

    Create checkpoint1.swift

---
+++ checkpoint1.swift
@@ -0,0 +1,9 @@
+/*
+ This program will create a temperature converter which will convert a temperature constant from Celsius to Fahrenheit.
+ */
+
+let celsiusTemp = 25.0
+let fahrenheitTemp = (celsiusTemp * 9)/5 + 32.0
+
+print("Celsius Temperature is \(celsiusTemp)°")
+print("Fahrenheit Temperature is \(fahrenheitTemp)°")
