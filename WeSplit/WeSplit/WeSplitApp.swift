552db0b44deeeca6d4ef03607dd94b7c30af4599
Lexi Han <108366350+lhapollo@users.noreply.github.com>
2024-04-15 18:08:04 -0400

    Add files via upload
    Project 1: WeSplit
    Tutorial: 100 Days of SwiftUI

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Lexi Han on 2024-04-13.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
