d70e11134fe70b67b03a9632a4b491a7e50531f9
Lexi Han <108366350+lhapollo@users.noreply.github.com>
2024-04-19 16:32:18 -0400

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Lexi Han on 2024-04-17.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
