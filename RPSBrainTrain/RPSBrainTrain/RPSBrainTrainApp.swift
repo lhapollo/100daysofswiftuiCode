34f31f754c7856c31532cbc8d530e54ddaff904c
Lexi Han <108366350+lhapollo@users.noreply.github.com>
2024-04-22 19:55:32 -0400

    Add files via upload

---
+++ RPSBrainTrain/RPSBrainTrain/RPSBrainTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSBrainTrainApp.swift
+//  RPSBrainTrain
+//
+//  Created by Lexi Han on 2024-04-22.
+//
+
+import SwiftUI
+
+@main
+struct RPSBrainTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
