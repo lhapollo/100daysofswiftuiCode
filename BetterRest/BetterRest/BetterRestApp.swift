1dff16782e536ff00d602754ee9b370d54dcea80
Lexi Han <108366350+lhapollo@users.noreply.github.com>
2024-04-25 17:14:53 -0400

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Lexi Han on 2024-04-23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
