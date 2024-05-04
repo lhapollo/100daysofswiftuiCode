1f612febd61b32f8a22fe45472586c858b4756d1
Lexi Han <108366350+lhapollo@users.noreply.github.com>
2024-05-04 09:14:36 -0400

    Add files via upload

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Lexi Han on 2024-05-03.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
