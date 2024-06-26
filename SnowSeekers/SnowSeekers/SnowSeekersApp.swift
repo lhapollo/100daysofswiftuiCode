87dc38beafc89469fc6eafe52a78fa05befccfe1
Lexi Han <108366350+lhapollo@users.noreply.github.com>
2024-06-26 10:27:19 -0400

    Add files via upload

---
+++ SnowSeekers/SnowSeekers/SnowSeekersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekersApp.swift
+//  SnowSeekers
+//
+//  Created by Lexi Han on 2024-06-26.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnowSeekers/SnowSeekers/WelcomeView.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeView.swift
+//  SnowSeekers
+//
+//  Created by Lexi Han on 2024-06-26.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to SnowSeeker!")
+                .font(.largeTitle)
+            Text("Please select a resort from the left-hand menu; swipe from the left edge to show it.")
+                .foregroundStyle(.secondary)
+        }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
